87795924180b56984fa1c25ad660fc40e89d285c
theOpenbit <tob@schoenesnetz.de>
2017-03-14 19:23:41 +0100

    removed unsed lines

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ cd docker-nfs-server
 sed -i '1s/.*/FROM resin\/rpi-raspbian:jessie/' Dockerfile
 # Avoid ERROR: invoke-rc.d: policy-rc.d denied execution of start. 
 sed -i '3 i\RUN echo "#!/bin/sh\\nexit 0" > /usr/sbin/policy-rc.d' Dockerfile
-#sed -i 's/RUN apt-get update -qq \&\& apt-get install -y nfs-kernel-server runit inotify-tools -qq/RUN apt-get update -qq \&\& apt-get install -y nfs-kernel-server inotify-tools/g' Dockerfile
-#sed -i 's/exec runsvdir \/etc\/sv/exec service rpcbind restart \&\& service nfs-kernel-server start/g' nfs_setup.sh
 DOCKER_TAG='latest'
 if [ -n "$TRAVIS_TAG" ]; then DOCKER_TAG=$TRAVIS_TAG; fi
 docker build -t theopenbit/rpi-nfs-server:$DOCKER_TAG .
